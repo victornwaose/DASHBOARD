158420566352aa886876556bf016bf5df794bd09
nwaose victor <nwaosevictor1234@gmail.com>
2021-09-15 13:03:53 +0100

    added productlist

--- src/components/pages/product/Product.js
+++ src/components/pages/product/Product.js
@@ -3,6 +3,7 @@ import "./Product.css";
 import {Link} from "react-router-dom";
 import Chart from '../../chart/Chart';
 import { productData } from '../../../data';
+import { Publish } from '@material-ui/icons';
 
 const Product = () => {
     return (
@@ -15,18 +16,62 @@ const Product = () => {
            </div>
            <div className="productTop">
                 <div className="productTopLeft">
-                    <Chart data={productData} dataKey="Sales" title="Sales P erformance" />
+                    <Chart data={productData} dataKey="Sales" title="Sales Performance" />
                 </div>
                 <div className="productTopRight">
                     <div className="productInfoTop">
-                        <img src="https://images.unsplash.com/photo-1588872657578-7efd1f1555ed"  alt="" />
+                        <img src="https://images.unsplash.com/photo-1588872657578-7efd1f1555ed"  alt="" className=" " />
                         <span className="productName">Laptop</span>
                     </div>
-                    <div className="productInfoBottom"></div>
+                    <div className="productInfoBottom">
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">id:</span>
+                            <span className="productInfoValue">123</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">Sales:</span>
+                            <span className="productInfoValue">4523</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">active:</span>
+                            <span className="productInfoValue">yes</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">In Stock:</span>
+                            <span className="productInfoValue">No</span>
+                        </div>
+                    </div> 
                 </div>
            </div>
-           <div className="productBottom"></div>
-        </div>
+           <div className="productBottom">
+               <form className="productForm">
+                    <div className="productFormLeft">
+                        <label>Product Name</label>
+                        <input type="text" placeholder="Laptop" /> 
+                        <label>In Stock</label>
+                        <select name="InStock" id="idStock">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                        <label>Active</label>
+                        <select name="Active" id="Active">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </div>
+                    <div className="productFormRight">
+                        <div className="productUpLoad">
+                            <img className="productUpLoadImg" src="https://images.unsplash.com/photo-1588872657578-7efd1f1555ed" alt=""  />
+                            <label   htmlFor="file">
+                                <Publish />
+                            </label>
+                            <input type="file" id="file" style={{display:"none"}} />
+                        </div>
+                        <button className="productButton">Update</button>
+                    </div>
+               </form>
+           </div>
+        </div> 
     )
 }
 
